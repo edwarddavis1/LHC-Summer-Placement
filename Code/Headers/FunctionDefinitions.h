167133ddd9e2c8959109900027f2275d041e814d
edwarddavis1 <52565961+edwarddavis1@users.noreply.github.com>
2019-07-05 11:04:16 +0100

    Create FunctionDefinitions
    Need to declare any functions which will need to be later defined in a class.

---
+++ Code/Headers/FunctionDefinitions.h
@@ -0,0 +1,6 @@
+virtual void print_variable(); //simply for proof of concept
+
+//virtual void particle_selection();
+virtual bool event_pair_truth();
+virtual bool initial_cuts_truth();
+virtual string event_type();
