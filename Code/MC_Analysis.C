688a0b8ecbf878e1aaf85e79fe94a81daeecf036
edwarddavis1 <52565961+edwarddavis1@users.noreply.github.com>
2019-07-05 11:01:52 +0100

    Create MC_Analysis.C
    The main file to be called to run the main program. 
    Included in this file is the main loop where all user defined functions are called in order to perform the analysis.

---
+++ Code/MC_Analysis.C
@@ -0,0 +1,234 @@
+#define MC_Analysis_cxx
+#include "Headers/MC_Analysis.h"
+#include <TH2.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+
+// For Loading percentages
+float prog, load;
+int prog_int, last_prog=0;
+
+
+void MC_Analysis::Loop()
+{
+	if (fChain == 0) return;			// If the chain is empty, end the loop
+
+	Long64_t nentries = fChain->GetEntriesFast();	// Get the number of entries
+	Long64_t nbytes = 0, nb = 0;			// Set number of bytes to zero
+
+
+	/////////////////////////////////////////////////////////////////////////////////
+	//------------------------------ Book Hists here ------------------------------//
+	/////////////////////////////////////////////////////////////////////////////////
+	#include "Headers/BookHistos.h"
+
+	/////////////////////////////////////////////////////////////////////
+	///------------------ FOR LOOP USED IN ANALYSIS ------------------///
+	/////////////////////////////////////////////////////////////////////
+
+	for (Long64_t jentry=0; jentry<nentries;jentry++ and load++) {
+		
+		/////////////////////////////////////////////////////////////////////
+		///-------------------- DATA EXISTANCE CHECKS --------------------///
+		/////////////////////////////////////////////////////////////////////
+
+		Long64_t ientry = LoadTree(jentry);		// Load the entry in the tree with number jentry
+		if (ientry < 0) break;				// If there is no entry, break the loop
+		nb = fChain->GetEntry(jentry);   nbytes += nb;	// Add the position in bytes to nbytes 
+		
+		//////////////////////////////////////////////////////////////////////		
+		///--------------------------- ANALYSIS ---------------------------///
+		//////////////////////////////////////////////////////////////////////
+
+		// Loading Percentages //
+		prog = (load / nentries)*10;
+		prog_int = (int)round(prog);
+		if (prog_int != last_prog) {
+			cout << prog_int*10-10 << "%" << endl;	
+			last_prog = prog_int;
+		}
+
+		double elec_0_pt = elec_0_p4->Pt();
+		double elec_1_pt = elec_1_p4->Pt();
+		double muon_0_pt = muon_0_p4->Pt();
+		double muon_1_pt = muon_1_p4->Pt();
+		double tau_0_pt = tau_0_p4->Pt();
+		double tau_1_pt = tau_1_p4->Pt();
+
+		// Event identification
+		string lep_type;
+		if (elec_0 > 0) {
+			lep_type = "Electron";
+			h_lep_type->Fill("Electron", elec_0);
+		}
+		else if (muon_0 > 0) {
+			lep_type = "Muon";
+			h_lep_type->Fill("Muon", muon_0);
+		}
+		else if (tau_0 > 0) {
+			lep_type = "Tau";
+			h_lep_type->Fill("Tau", tau_0);
+		}
+
+
+		// Apply selection cuts & fill
+		bool event_pair = event_pair_truth();		// True if event has lepton-antilepton pair		
+		bool passed_cuts = initial_cuts_truth();	// True if selection cuts are passed
+
+		if (event_pair == true) {
+			if (passed_cuts == true){	
+				double elec_inv_mass = InvariantMass(elec_0_p4, elec_1_p4);
+				double muon_inv_mass = InvariantMass(muon_0_p4, muon_1_p4);
+				double tau_inv_mass = InvariantMass(tau_0_p4, tau_1_p4);
+				h_elec_inv_mass->Fill(elec_inv_mass);	
+				h_muon_inv_mass->Fill(muon_inv_mass);
+				h_tau_inv_mass->Fill(tau_inv_mass);
+
+				h_elec_pt->Fill(elec_0_pt);
+				h_muon_pt->Fill(muon_0_pt);	
+				h_tau_pt->Fill(tau_0_pt);
+
+				h_elec_iso_etcone20->Fill(elec_0_iso_etcone20);
+				h_muon_iso_etcone20->Fill(muon_0_iso_etcone20);
+
+				h_elec_iso_ptcone30->Fill(elec_0_iso_ptcone30);
+				h_muon_iso_ptcone30->Fill(muon_0_iso_ptcone30);
+
+				h_ljet_inv_mass->Fill(InvariantMass(ljet_0_p4, ljet_1_p4));
+				h_bjet_inv_mass->Fill(InvariantMass(bjet_0_p4, bjet_1_p4));
+
+			}
+			else {
+				double elec_inv_mass_FC = InvariantMass(elec_0_p4, elec_1_p4);
+				double muon_inv_mass_FC = InvariantMass(muon_0_p4, muon_1_p4);
+				double tau_inv_mass_FC = InvariantMass(tau_0_p4, tau_1_p4);
+				h_elec_inv_mass_FC->Fill(elec_inv_mass_FC);	
+				h_muon_inv_mass_FC->Fill(muon_inv_mass_FC);
+				h_tau_inv_mass_FC->Fill(tau_inv_mass_FC);
+
+				h_elec_pt_FC->Fill(elec_0_pt);
+				h_muon_pt_FC->Fill(muon_0_pt);	
+				h_tau_pt_FC->Fill(tau_0_pt);
+
+				h_elec_iso_etcone20_FC->Fill(elec_0_iso_etcone20);
+				h_muon_iso_etcone20_FC->Fill(muon_0_iso_etcone20);
+
+				h_elec_iso_ptcone30_FC->Fill(elec_0_iso_ptcone30);
+				h_muon_iso_ptcone30_FC->Fill(muon_0_iso_ptcone30);
+
+				h_ljet_inv_mass_FC->Fill(InvariantMass(ljet_0_p4, ljet_1_p4));
+				h_bjet_inv_mass_FC->Fill(InvariantMass(bjet_0_p4, bjet_1_p4));
+
+			}
+		}
+		
+		// Number of leptons
+		int lep_n;
+		h_lep_n->Fill(lep_n);
+
+		// Jets
+		h_n_jets->Fill(n_jets);
+		
+		h_ljet_0_pt->Fill(ljet_0_p4->Pt());
+		h_ljet_0_eta->Fill(ljet_0_p4->Eta());
+		h_ljet_0_phi->Fill(ljet_0_p4->Phi());
+		
+		h_bjet_0_pt->Fill(bjet_0_p4->Pt());
+		h_bjet_0_eta->Fill(bjet_0_p4->Eta());
+		h_bjet_0_phi->Fill(bjet_0_p4->Phi());
+
+	}
+	
+	// Set style for histograms //
+	h_elec_pt->SetLineColor(8);			// pT
+	h_elec_pt->GetXaxis()->SetTitle("pT (GeV)");
+	h_elec_pt->GetYaxis()->SetTitle("Counts");
+	
+	h_muon_pt->SetLineColor(8);
+	h_muon_pt->GetXaxis()->SetTitle("pT (GeV)");
+	h_muon_pt->GetYaxis()->SetTitle("Counts");
+
+	h_tau_pt->SetLineColor(8);
+	h_tau_pt->GetXaxis()->SetTitle("pT (GeV)");
+	h_tau_pt->GetYaxis()->SetTitle("Counts");
+
+	// Create file to write histograms	
+	//TFile *hfile = gROOT->FindObject("hsimple.root"); if (hfile) hfile->Close();
+  	//hfile = new TFile("hsimple.root","RECREATE","Demo ROOT file with histograms");
+	TFile outfile("OutputFiles.root","RECREATE");
+
+	h_lep_n->Write();
+
+	h_lep_type->Write();
+
+	h_elec_pt->Write();
+	h_muon_pt->Write();
+	h_tau_pt->Write();
+	h_elec_pt_FC->Write();
+	h_muon_pt_FC->Write();
+	h_tau_pt_FC->Write();
+
+	h_elec_inv_mass->Write();
+	h_muon_inv_mass->Write();
+	h_tau_inv_mass->Write();
+	h_elec_inv_mass_FC->Write();
+	h_muon_inv_mass_FC->Write();
+	h_tau_inv_mass_FC->Write();
+
+	
+	h_elec_iso_etcone20->Write();
+	h_muon_iso_etcone20->Write();
+	h_elec_iso_etcone20_FC->Write();
+	h_muon_iso_etcone20_FC->Write();
+
+	h_elec_iso_ptcone30->Write();
+	h_muon_iso_ptcone30->Write();
+	h_elec_iso_ptcone30_FC->Write();
+	h_muon_iso_ptcone30_FC->Write();
+
+	h_n_jets->Write();
+	
+	h_ljet_0_pt->Write();
+	h_ljet_0_eta->Write();
+	h_ljet_0_phi->Write();
+
+	h_bjet_0_pt->Write();
+	h_bjet_0_eta->Write();
+	h_bjet_0_phi->Write();
+
+	h_ljet_inv_mass->Write();
+	h_bjet_inv_mass->Write();
+	h_ljet_inv_mass_FC->Write();
+	h_bjet_inv_mass_FC->Write();
+
+	outfile.Close();
+
+	// Opens a new TBrowser without having to ask the terminal manually
+	gROOT->ProcessLine("new TBrowser");
+	
+}
+
+
+//   In a ROOT session, you can do:
+//      Root > .L myAnalysis.C
+//      Root > myAnalysis t
+//      Root > t.GetEntry(12); // Fill t data members with entry number 12
+//      Root > t.Show();       // Show values of entry 12
+//      Root > t.Show(16);     // Read and show values of entry 16
+//      Root > t.Loop();       // Loop on all entries
+//
+
+//     This is the loop skeleton where:
+//    jentry is the global entry number in the chain
+//    ientry is the entry number in the current Tree
+//  Note that the argument to GetEntry must be:
+//    jentry for TChain::GetEntry
+//    ientry for TTree::GetEntry and TBranch::GetEntry
+//
+//       To read only selected branches, Insert statements like:
+// METHOD1:
+//    fChain->SetBranchStatus("*",0);  // disable all branches
+//    fChain->SetBranchStatus("branchname",1);  // activate branchname
+// METHOD2: replace line
+//    fChain->GetEntry(jentry);       //read all branches
+//by  b_branchname->GetEntry(ientry); //read only this branch
