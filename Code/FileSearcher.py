f7a98a5d14a3d32aa85efdbe3c64d4c9201655e7
edwarddavis1 <52565961+edwarddavis1@users.noreply.github.com>
2019-07-10 13:13:17 +0100

    Update FileSearcher.py
    Will now run multiple analysis files, automatically getting the file paths depending on a specific sub-string.
    The code cannot yet combine multiple datasets and can sometimes run the same simulation twice (higgs ssh).

--- Code/FileSearcher.py
+++ Code/FileSearcher.py
@@ -1,3 +1,4 @@
+import ROOT as r
 import os
 from subprocess import PIPE,Popen
 
@@ -9,28 +10,73 @@ print("Getting data from computer: %s..."%host)
 
 if host == "higgs":
 	MC_path = "/higgs-data3/sam/forTomRyunAliceLuca/v03/mc"
+	specifics_start = 6	# specifics_start refers to the amount of "."'s before the substring telling you the specifics of the dataset
 elif host == "pc2014":
 	MC_path = "/pc2014-data4/sam/VBF_Ztt/HIGG8D1/v5.0/mc"
+	specifics_start = 7
 elif host == "pc2012":
 	MC_path = "/pc2012-data1/sam/VBF_Ztt/HIGG8D1/v5.0/mc"
+	specifics_start = 7
 	
-found_dir = False	# Used to check whether a directory was found or not	
+found_dir = False	# Used to check whether a directory was found or not
+file_paths = []	
+file_specifics = []
 
 while found_dir == False:
 	choice = raw_input("Enter which process you want to analyse: ")
+	#choice = "Zee2jets"
+	print("Here's what I found:")
 
 	# Searches the MC directory for sub-directories containing strings of what the user entered
 	# Then prints out the paths to all files within the found directory
 	for dirnames in os.walk(MC_path):
 		for dirname in dirnames:
 			if choice in dirname:	
-				print("Here's what I found:")
-				#print(dirname)					# Gives the path of the chosen MC directory
 				found_dir = True
 				for filenames in os.walk(dirname):
+										
+					# Writes out the full file path
 					filename = filenames[2][0]
-					#print(filename)			# Gives the name of the files within the chosen MC directory
-					print("\n"+dirname+"/"+filename)	# Writes out the full file path
+					file_path = dirname+"/"+filename
+					
+					# Finds out the specifics of the process e.g. Zee_MV0_70_CVetBVet
+					file_specific = "_".join(file_path.split(".")[specifics_start].split("_")[2:]) 
+					print("\n"+file_specific)
+					print(file_path)	
+					file_paths.append(file_path)
+					file_specifics.append(file_specific)	
+									
+					# Analyse this file
+					with open(os.path.join("Headers","ChosenFile.h"), "w") as text_file:
+						text_file.write("decay_chain_file = "+'"%s";\nchoice = "%s";'%(file_path,file_specific))
+										
 	if found_dir == False:
 		print("I'm sorry, I couldn't find a matching simulation!")
+
+
+
+# Analysis 
+print("\nBegining analysis...")
+for i in range(len(file_paths)):
+	print("Analysing %s, %i/%i"%(file_specifics[i],i+1,len(file_paths)))
+	with open(os.path.join("Headers","ChosenFile.h"), "w") as text_file:
+		text_file.write("decay_chain_file = "+'"%s";\nchoice = "%s";'%(file_paths[i],file_specifics[i]))
+
+	r.gROOT.Reset()
+	r.gROOT.ProcessLine(".L MC_Analysis.C")
+	r.gROOT.ProcessLine("MC_Analysis t")
+	r.gROOT.ProcessLine("t.Loop();")
+	
+	if i != len(file_paths)-1:
+		r.gROOT.ProcessLine(".q")
+	else:
+		r.gROOT.ProcessLine("new TBrowser")
+		os.system("root -l")
+
+
+
+
+
+
+
 
