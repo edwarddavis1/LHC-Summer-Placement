44ba197bbfd2db68f10c07cbe3bbef4203cf436f
edwarddavis1 <52565961+edwarddavis1@users.noreply.github.com>
2019-07-11 14:59:00 +0100

    Update BookHistos.h
    Changed to work better with the working FileSearcher.py script
    - Added modular histogram titles
    - Removed auto-scaling axes as histograms will not properly combine otherwise

--- Code/Headers/BookHistos.h
+++ Code/Headers/BookHistos.h
@@ -22,27 +22,27 @@ TH1F* h_elec_muon_inv_mass_FC = new TH1F("inv_mass_elec_muon_FC", TString::Forma
 TH1F* h_muon_tau_inv_mass_FC = new TH1F("inv_mass_muon_elec_FC", TString::Format("Dilepton Invariant Mass from MuonTau in MC %s Failed Cuts",choice.c_str()), 500, 0, 150);
 TH1F* h_elec_tau_inv_mass_FC = new TH1F("inv_mass_elec_tau_FC", TString::Format("Dilepton Invariant Mass from TauElectron in MC %s Failed Cuts",choice.c_str()), 500, 0, 150);
 /*
-TH1F* h_elec_iso_etcone20 = new TString::Format("elec_etcone20", "MC %s Electrons etcone20", 500, -20, 100);
-TH1F* h_muon_iso_etcone20 = new TH1F("muon_etcone20", "MC %s Muons etcone20", 500, -20, 100);
-TH1F* h_elec_iso_etcone20_FC = new TH1F("elec_etcone20_FC", "MC %s Electrons etcone20 Failed Cuts", 500, -2000, 100000);
-TH1F* h_muon_iso_etcone20_FC = new TH1F("muon_etcone20_FC", "MC %s Muons etcone20 Failed Cuts", 500, -2000, 100000);
+TH1F* h_elec_iso_etcone20 = new TString::Format("elec_etcone20", TString::Format("MC %s Electrons etcone20",choice.c_str()), 500, -20, 100);
+TH1F* h_muon_iso_etcone20 = new TH1F("muon_etcone20", TString::Format("MC %s Muons etcone20",choice.c_str()), 500, -20, 100);
+TH1F* h_elec_iso_etcone20_FC = new TH1F("elec_etcone20_FC", TString::Format("MC %s Electrons etcone20 Failed Cuts",choice.c_str()), 500, -2000, 100000);
+TH1F* h_muon_iso_etcone20_FC = new TH1F("muon_etcone20_FC", TString::Format("MC %s Muons etcone20 Failed Cuts",choice.c_str()), 500, -2000, 100000);
 
-TH1F* h_elec_iso_ptcone30 = new TH1F("elec_ptcone30", "MC %s Electrons ptcone30", 500, -2000, 100000);
-TH1F* h_muon_iso_ptcone30 = new TH1F("muon_ptcone30", "MC %s Muons ptcone30", 500, -2000, 100000);
-TH1F* h_elec_iso_ptcone30_FC = new TH1F("elec_ptcone30_FC", "MC %s Electrons ptcone30 Failed Cuts", 500, -2000, 100000);
-TH1F* h_muon_iso_ptcone30_FC = new TH1F("muon_ptcone30_FC", "MC %s Muons ptcone30 Failed Cuts", 500, -2000, 100000);
+TH1F* h_elec_iso_ptcone30 = new TH1F("elec_ptcone30", TString::Format("MC %s Electrons ptcone30",choice.c_str()), 500, -2000, 100000);
+TH1F* h_muon_iso_ptcone30 = new TH1F("muon_ptcone30", TString::Format("MC %s Muons ptcone30",choice.c_str()), 500, -2000, 100000);
+TH1F* h_elec_iso_ptcone30_FC = new TH1F("elec_ptcone30_FC", TString::Format("MC %s Electrons ptcone30 Failed Cuts",choice.c_str()), 500, -2000, 100000);
+TH1F* h_muon_iso_ptcone30_FC = new TH1F("muon_ptcone30_FC", TString::Format("MC %s Muons ptcone30 Failed Cuts",choice.c_str()), 500, -2000, 100000);
 */
-TH1F* h_n_jets = new TH1F("n_jets", "MC %s Number of Jets", 500, 0, 0);
+TH1F* h_n_jets = new TH1F("n_jets", TString::Format("MC %s Number of Jets",choice.c_str()), 500, 0, 10);
 
-TH1F* h_ljet_0_pt = new TH1F("ljet_0_pt", "MC %s Leading Leptonic Jets pT", 500, 0, 0);
-TH1F* h_ljet_0_eta = new TH1F("ljet_0_eta", "MC %s Leading Leptonic Jets Eta", 500, 0, 0);
-TH1F* h_ljet_0_phi = new TH1F("ljet_0_phi", "MC %s Leading Leptonic Jets Phi", 500, 0, 0);
+TH1F* h_ljet_0_pt = new TH1F("ljet_0_pt", TString::Format("MC %s Leading Leptonic Jets pT",choice.c_str()), 500, 0, 1000);
+TH1F* h_ljet_0_eta = new TH1F("ljet_0_eta", TString::Format("MC %s Leading Leptonic Jets Eta",choice.c_str()), 500, 0, 5);
+TH1F* h_ljet_0_phi = new TH1F("ljet_0_phi", TString::Format("MC %s Leading Leptonic Jets Phi",choice.c_str()), 500, 0, 4);
 
-TH1F* h_bjet_0_pt = new TH1F("bjet_0_pt", "MC %s Leading b Jets pT", 500, 0, 0);
-TH1F* h_bjet_0_eta = new TH1F("bjet_0_eta", "MC %s Leading b Jets Eta", 500, 0, 0);
-TH1F* h_bjet_0_phi = new TH1F("bjet_0_phi", "MC %s Leading b Jets Phi", 500, 0, 0);
+TH1F* h_bjet_0_pt = new TH1F("bjet_0_pt", TString::Format("MC %s Leading b Jets pT",choice.c_str()), 500, 0, 1000);
+TH1F* h_bjet_0_eta = new TH1F("bjet_0_eta", TString::Format("MC %s Leading b Jets Eta",choice.c_str()), 500, 0, 5);
+TH1F* h_bjet_0_phi = new TH1F("bjet_0_phi", TString::Format("MC %s Leading b Jets Phi",choice.c_str()), 500, 0, 4);
 
-TH1F* h_ljet_inv_mass = new TH1F("ljet_inv_mass", "MC %s Leptonic Jets Invariant Mass", 500, 0, 1000);
-TH1F* h_bjet_inv_mass = new TH1F("bjet_inv_mass", "MC %s b Jets Invariant Mass", 500, 0, 1000);
-TH1F* h_ljet_inv_mass_FC = new TH1F("ljet_inv_mass_FC", "MC %s Leptonic Jets Invariant Mass Failed Cuts", 500, 0, 1000);
-TH1F* h_bjet_inv_mass_FC = new TH1F("bjet_inv_mass_FC", "MC %s b Jets Invariant Mass Failed Cuts", 500, 0, 1000);
+TH1F* h_ljet_inv_mass = new TH1F("ljet_inv_mass", TString::Format("MC %s Leptonic Jets Invariant Mass",choice.c_str()), 500, 0, 1000);
+TH1F* h_bjet_inv_mass = new TH1F("bjet_inv_mass", TString::Format("MC %s b Jets Invariant Mass",choice.c_str()), 500, 0, 1000);
+TH1F* h_ljet_inv_mass_FC = new TH1F("ljet_inv_mass_FC", TString::Format("MC %s Leptonic Jets Invariant Mass Failed Cuts",choice.c_str()), 500, 0, 1000);
+TH1F* h_bjet_inv_mass_FC = new TH1F("bjet_inv_mass_FC", TString::Format("MC %s b Jets Invariant Mass Failed Cuts",choice.c_str()), 500, 0, 1000);
