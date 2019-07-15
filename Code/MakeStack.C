2028596c844c4fb9c316df331eaacb2accbcd968
edwarddavis1 <52565961+edwarddavis1@users.noreply.github.com>
2019-07-15 16:59:37 +0100

    Update MakeStack.C
    Created a summed stack of Zee_MV70-140

--- Code/MakeStack.C
+++ Code/MakeStack.C
@@ -1,32 +1,39 @@
 void MakeStack() {
 	//--------------------- Stacking accross datasets ----------------------//
 	cout << "Making stacks..." << endl;
-	TCanvas *c_elec_inv_mass = new TCanvas("elec_inv_mass");
-	THStack *h_elec_inv_mass_stack = new THStack("elec_inv_mass_stack", "Electron Invariant Mass Test Stack");
+	TCanvas *c_elec_inv_mass = new TCanvas("elec_inv_mass_Zee_MV70-140");
+	THStack *h_elec_inv_mass_stack = new THStack("elec_inv_mass_Zee_MV70-140_stack", "Electron Invariant Mass ZeeMV70-140");
 
 	// Read in histogram from other datasets
 	TFile* Zee2jets_file = new TFile("OutputFiles/Zee2jets_Min_N_TChannel.root");
 	TH1F* h_Zee2jets_elec_inv_mass = (TH1F*)Zee2jets_file->Get("elec_inv_mass");
 	TH1F* h_Zee2jets_elec_inv_mass_NoCuts = (TH1F*)Zee2jets_file->Get("elec_inv_mass_NoCut");
+	
+	TFile* Zee_MV70_140_file = new TFile("OutputFiles/Zee_MV70-140.root");
+	TH1F* h_Zee_MV70_140_elec_inv_mass = (TH1F*)Zee_MV70_140_file->Get("elec_inv_mass");
+	TH1F* h_Zee_MV70_140_elec_inv_mass_NoCuts = (TH1F*)Zee_MV70_140_file->Get("elec_inv_mass_NoCut");
 
-	TFile* Zee_MV0_file = new TFile("OutputFiles/Zee_MV0_70_CFilBVet.root");
-	TH1F* h_Zee_MV0_elec_inv_mass = (TH1F*)Zee_MV0_file->Get("elec_inv_mass");	
+	//TFile* Zee_MV0_file = new TFile("OutputFiles/Zee_MV0_70_CFilBVet.root");
+	//TH1F* h_Zee_MV0_elec_inv_mass = (TH1F*)Zee_MV0_file->Get("elec_inv_mass");	
 	
-	h_Zee2jets_elec_inv_mass->SetFillColor(kBlue);
-	h_Zee2jets_elec_inv_mass_NoCuts->SetFillColor(kCyan);
-	h_Zee_MV0_elec_inv_mass->SetLineColor(4);	
+	//h_Zee2jets_elec_inv_mass->SetFillColor(kMagenta);
+	//h_Zee2jets_elec_inv_mass_NoCuts->SetFillColor(kMagenta-10);
+
+	h_Zee_MV70_140_elec_inv_mass->SetFillColor(kBlue);
+	h_Zee_MV70_140_elec_inv_mass_NoCuts->SetFillColor(kCyan);	
 	
-	h_elec_inv_mass_stack->Add(h_Zee2jets_elec_inv_mass);
-	h_elec_inv_mass_stack->Add(h_Zee2jets_elec_inv_mass_NoCuts);
-	h_elec_inv_mass_stack->Add(h_Zee_MV0_elec_inv_mass);
+	//h_elec_inv_mass_stack->Add(h_Zee2jets_elec_inv_mass);
+	//h_elec_inv_mass_stack->Add(h_Zee2jets_elec_inv_mass_NoCuts);
+	h_elec_inv_mass_stack->Add(h_Zee_MV70_140_elec_inv_mass);
+	h_elec_inv_mass_stack->Add(h_Zee_MV70_140_elec_inv_mass_NoCuts);
 
 	h_elec_inv_mass_stack->Draw();
 	gPad->BuildLegend(0.75,0.75,0.95,0.95,"");
 
-	TFile StackFile("StackFile.root","RECREATE");
+	TFile outfile("StackFile","RECREATE");
 	c_elec_inv_mass->Write();
-	h_Zee2jets_elec_inv_mass->Write();
-	StackFile.Close();
+	h_Zee_MV70_140_elec_inv_mass->Write();
+	outfile.Close();
 	cout << "Stacks made!" << endl;
 	
 }
