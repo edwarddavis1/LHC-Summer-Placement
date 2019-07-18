025d5281d94dc1fd923bb026bdd313254a2229f6
edwarddavis1 <52565961+edwarddavis1@users.noreply.github.com>
2019-07-18 14:42:31 +0100

    Update MakeStack.C
    Makes stacks for any/all Zmumu processes

--- Code/MakeStack.C
+++ Code/MakeStack.C
@@ -1,16 +1,24 @@
 #define MakeStack_cxx
 #include "Headers/MC_Analysis.h"
 
-void AddStack(TString c_name, TString get_hist, TString files[]) {
+void AddStack(TString c_name, TString get_hist, TString files[], int file_length,bool norm_hist=false) {
 	TCanvas *c = new TCanvas(c_name);
 	THStack *hs = new THStack(c_name+"_stack","");
-	auto legend = new TLegend(0.75,0.8,0.99,0.99);
+	auto legend = new TLegend(0.75,0.7,0.99,0.99);
+	TString leg_title;
 	
-	int file_length = files->Length();
 	for (UInt_t i=0;i<file_length;i++){
 		TFile* file = new TFile("OutputFiles/"+files[i]+".root");
 		TH1F* h = (TH1F*)file->Get(get_hist);
-		h->SetFillColor(i+2);
+		if (norm_hist == true) {
+			Double_t norm = h->GetEntries();	// Normalise histogram 
+			h->Scale(1/norm);
+			h->SetLineColor(i+2);
+			leg_title = get_hist+"_norm";
+			cout << leg_title << endl;
+		}
+		else h->SetFillColor(i+2);
+		leg_title=get_hist;
 		//h->SetFillStyle(3003);
 		hs->Add(h);
 		legend->AddEntry(h,files[i],"f");
@@ -21,29 +29,34 @@ void AddStack(TString c_name, TString get_hist, TString files[]) {
 	gPad->Modified();
 	gPad->Update();	
 	
-	legend->SetHeader(get_hist,"C"); // option "C" allows to center the header
+	legend->SetHeader(leg_title,"C"); // option "C" allows to center the header
 	legend->SetTextSize(0.02);
 	legend->Draw();
 	}
 	
-	TFile outfile("OutputFiles/StackedHists.root","RECREATE");
-	c->Write();
+	TFile outfile("OutputFiles/StackedHists.root","UPDATE");
+	c->Write("",TObject::kOverwrite);
 	outfile.Close();
 }
+
 
 
 void MakeStack() {
 	cout << "Making stacks..." << endl;
 
+	// All Zmumu datasets
 	TString files[]={"Zmm2jets_Min_N_TChannel","Zmumu_MV70_140_BFil","Zmumu_MV70_140_CFilBVet","Zmumu_MV70_140_CVetBVet","Zmumu_MV140_280_BFil",
-			"Zmumu_MV140_280_CFilBVet","Zmumu_MV140_280_CVetBVet"};
-	AddStack("eta_mumu","lep_eta",files);
-
-	
-	
-
+			"Zmumu_MV140_280_CFilBVet","Zmumu_MV140_280_CVetBVet", "Zmumu_MV0_70_BFil","Zmumu_MV0_70_CFilBVet","Zmumu_MV0_70_CVetBVet",
+			"Zmumu_MV280_500_BFil","Zmumu_MV280_500_CFilBVet","Zmumu_MV280_500_CVetBVet","Zmumu_MV500_1000","Zmumu_MV1000_E_CMS"};
+			
+	bool norm_hist=true;
+	AddStack("eta_mumu_norm","lep_eta",files,15,norm_hist);
+	AddStack("eta_mumu","lep_eta",files,15);
+	//AddStack("inv_mass_mumu_norm","muon_inv_mass",files,7,norm_hist);
+	//AddStack("inv_mass_mumu_NoCut_norm","muon_inv_mass_NoCut",files,7,norm_hist);
+	//AddStack("pt_mumu","lep_pt",files,7);
 	cout << "Stacks made!" << endl;
-	
+	/*
 	TCanvas* eta_diff = new TCanvas("eta_diff");
 	auto l1 = new TLegend(0.75,0.8,0.99,0.99);
 	
@@ -62,13 +75,6 @@ void MakeStack() {
 	l1->SetHeader("delta eta","C"); // option "C" allows to center the header
 	l1->SetTextSize(0.02);
 	l1->Draw();
-	
-	/*TFile *f1 = TFile::Open("…/Downloads/Run08042_ProjectionX.root");
-	TCanvas c1 = (TCanvas)f1->Get(c1”);
-	TH1D h1 = (TH1D)c1->FindObject(“cutg”);
-	TFile *f2 = TFile::Open("…/Downloads/Run08044_ProjectionX.root");
-	TCanvas c2 = (TCanvas)f1->Get(“c1”);
-	TH1D h2 = (TH1D)c2->FindObject(“cutg”);
-	h1->Add(h2,-1);*/
+	*/
 	
 }
