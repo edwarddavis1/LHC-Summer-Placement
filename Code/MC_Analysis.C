986ce76da16903fa158df375bb0fb8ccb4e94ef9
edwarddavis1 <52565961+edwarddavis1@users.noreply.github.com>
2019-07-29 12:54:54 +0100

    Update MC_Analysis.C
    Tidied up the code, added Filling of pt balance

--- Code/MC_Analysis.C
+++ Code/MC_Analysis.C
@@ -13,158 +13,248 @@ auto start = chrono::steady_clock::now();
 float prog, load;
 int prog_int, last_prog=0;
 
+int cut_entries=0;
 
 void MC_Analysis::Loop()
 {
-	if (fChain == 0) return;			// If the chain is empty, end the loop
+	// if the chain is empty, end the loop
+	if (fChain == 0) return;
 
-	Long64_t nentries = fChain->GetEntriesFast();	// Get the number of entries
-	Long64_t nbytes = 0, nb = 0;			// Set number of bytes to zero
+	// get the number of entries
+	Long64_t nentries = fChain->GetEntriesFast();
+	// set number of bytes to zero
+	Long64_t nbytes = 0, nb = 0;
 
 
-	/////////////////////////////////////////////////////////////////////////////////
-	//------------------------------ Book Hists here ------------------------------//
-	/////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	//--------------------------- Book Hists here ----------------------------//
+	////////////////////////////////////////////////////////////////////////////
 	#include "Headers/BookHistos.h"
-	
-	/////////////////////////////////////////////////////////////////////
-	///------------------ FOR LOOP USED IN ANALYSIS ------------------///
-	/////////////////////////////////////////////////////////////////////
+
+	////////////////////////////////////////////////////////////////////////////
+	///---------------------- FOR LOOP USED IN ANALYSIS ---------------------///
+	////////////////////////////////////////////////////////////////////////////
 
 	for (Long64_t jentry=0; jentry<nentries;jentry++ and load++) {
-		
-		/////////////////////////////////////////////////////////////////////
-		///-------------------- DATA EXISTANCE CHECKS --------------------///
-		/////////////////////////////////////////////////////////////////////
 
-		Long64_t ientry = LoadTree(jentry);		// Load the entry in the tree with number jentry
-		if (ientry < 0) break;				// If there is no entry, break the loop
-		nb = fChain->GetEntry(jentry);   nbytes += nb;	// Add the position in bytes to nbytes 
-		
-		//////////////////////////////////////////////////////////////////////		
-		///--------------------------- ANALYSIS ---------------------------///
-		//////////////////////////////////////////////////////////////////////
-		
+		////////////////////////////////////////////////////////////////////////
+		///--------------------- DATA EXISTANCE CHECKS ----------------------///
+		////////////////////////////////////////////////////////////////////////
+
+		// load the entry in the tree with number jentry
+		Long64_t ientry = LoadTree(jentry);
+		// if there is no entry, break the loop
+		if (ientry < 0) break;
+		// add the position in bytes to nbytes
+		nb = fChain->GetEntry(jentry);   nbytes += nb;
+
+		////////////////////////////////////////////////////////////////////////
+		///---------------------------- ANALYSIS ----------------------------///
+		////////////////////////////////////////////////////////////////////////
+
 		// Loading Percentages //
 		prog = (load / nentries)*10;
 		prog_int = (int)round(prog);
 		if (prog_int != last_prog) {
-			cout << prog_int*10-10 << "%" << endl;	
+			cout << prog_int*10-10 << "%" << endl;
 			last_prog = prog_int;
 		}
 
-		// Selects the lepton in the event and loads the data in to the lep_ variables
-		ParticleSelection();
-		event_type();
-		
-		#include "Headers/VariableExtraction.h"	// Extracts pt,eta,phi from 4-vectors
-		
-		
+		ParticleSelection();		// Selects lep_ variables
+		event_type();				// Sets lep_type
+
+		// extracts kinematic variables from 4-vectors
+		#include "Headers/VariableExtraction.h"
+
+
 
 		// Apply selection cuts & fill
-		bool event_pair = event_pair_truth();		// True if event has lepton-antilepton pair		
-		bool passed_cuts = initial_cuts_truth();	// True if selection cuts are passed
+		bool event_pair = event_pair_truth();			// Pre-selection cut
+		bool passed_cuts = initial_cuts_truth();		// Selection cut
 
 		if (event_pair == true) {
-			if (passed_cuts == true){	
-				double dilep_inv_mass = InvariantMass(lep_0_p4, lep_1_p4);			
-				if (lep_type == "Electron") h_elec_inv_mass->Fill(dilep_inv_mass);
-				if (lep_type == "Muon") h_muon_inv_mass->Fill(dilep_inv_mass);
-				if (lep_type == "Tau") h_tau_inv_mass->Fill(dilep_inv_mass);
-				if (lep_type == "ElectronMuon") h_elec_muon_inv_mass->Fill(dilep_inv_mass);
-				if (lep_type == "MuonTau") h_muon_tau_inv_mass->Fill(dilep_inv_mass);
-				if (lep_type == "ElectronTau") h_elec_tau_inv_mass->Fill(dilep_inv_mass);
-				
+			if (passed_cuts == true){
+
+				// --------------------- INVARIANT MASS --------------------- //
+				double dilep_inv_mass = InvariantMass(lep_0_p4, lep_1_p4);
+				if (lep_type == "Electron") {
+					h_elec_inv_mass->Fill(dilep_inv_mass);
+				} if (lep_type == "Muon") {
+					h_muon_inv_mass->Fill(dilep_inv_mass);
+				} if (lep_type == "Tau") {
+					h_tau_inv_mass->Fill(dilep_inv_mass);
+				} if (lep_type == "ElectronMuon") {
+					h_elec_muon_inv_mass->Fill(dilep_inv_mass);
+				} if (lep_type == "MuonTau") {
+					h_muon_tau_inv_mass->Fill(dilep_inv_mass);
+				} if (lep_type == "ElectronTau") {
+					h_elec_tau_inv_mass->Fill(dilep_inv_mass);
+				}
+				h_ljet_inv_mass->Fill(InvariantMass(ljet_0_p4, ljet_1_p4));
+				h_bjet_inv_mass->Fill(InvariantMass(bjet_0_p4, bjet_1_p4));
+
+
+				// ------------------------ CONES ----------------------------//
 				h_elec_iso_etcone20->Fill(elec_0_iso_etcone20);
 				h_muon_iso_etcone20->Fill(muon_0_iso_etcone20);
 
 				h_elec_iso_ptcone30->Fill(elec_0_iso_ptcone30);
 				h_muon_iso_ptcone30->Fill(muon_0_iso_ptcone30);
-				
-				h_ljet_inv_mass->Fill(InvariantMass(ljet_0_p4, ljet_1_p4));
-				h_bjet_inv_mass->Fill(InvariantMass(bjet_0_p4, bjet_1_p4));
-				
-				h_n_jets->Fill(n_jets);
-				
-				h_lep_pt->Fill(lep_0_pt);
-				h_lep_phi->Fill(lep_0_phi);
-				h_lep_eta->Fill(lep_0_eta);
-		
-				h_elec_pt->Fill(elec_0_pt);
-				h_elec_phi->Fill(elec_0_phi);
-				h_elec_eta->Fill(elec_0_eta);
-		
-				h_ljet_0_pt->Fill(ljet_0_pt);
+
+				// ---------------------- KINEMATICS ------------------------ //
+				h_lep_0_pt->Fill(lep_0_pt);						// Leptons
+				h_lep_0_phi->Fill(lep_0_phi);
+				h_lep_0_eta->Fill(lep_0_eta);
+				h_lep_rapidity->Fill(lep_0_p4->Rapidity());
+				h_lep_1_pt->Fill(lep_1_pt);
+				h_lep_1_phi->Fill(lep_1_phi);
+				h_lep_1_eta->Fill(lep_1_eta);
+				h_lep_rapidity->Fill(lep_1_p4->Rapidity());
+				double pt_balance = PtBalance(lep_0_p4, lep_1_p4,
+					 							ljet_0_p4, ljet_1_p4);
+				h_lep_pt_bal->Fill(pt_balance);
+
+				h_ljet_0_pt->Fill(ljet_0_pt);					// Light Jets
 				h_ljet_0_eta->Fill(ljet_0_eta);
 				h_ljet_0_phi->Fill(ljet_0_phi);
-				
 				h_ljet_1_pt->Fill(ljet_1_pt);
 				h_ljet_1_eta->Fill(ljet_1_eta);
 				h_ljet_1_phi->Fill(ljet_1_phi);
-		
-				h_bjet_0_pt->Fill(bjet_0_pt);
+
+				h_bjet_0_pt->Fill(bjet_0_pt);					// b Jets
 				h_bjet_0_eta->Fill(bjet_0_eta);
 				h_bjet_0_phi->Fill(bjet_0_phi);
-		
-				
-				// Rapidity interval
-				if (ljet_0_pt < ljet_1_pt) cout << "ljet_1 > ljet_0!!";
-				double delta_eta = ljet_0_eta - ljet_1_eta;
-				h_ljet_delta_eta->Fill(delta_eta);
-				
-				double Z_cent = Centrality(lep_0_p4, lep_1_p4, ljet_0_p4, ljet_1_p4);
+
+				h_lep_eta_sum->Fill(lep_0_eta + lep_1_eta);
+				h_ljet_eta_sum->Fill(ljet_0_eta + ljet_1_eta);
+
+				// angular separation
+				h_ljet_delta_eta->Fill(ljet_0_eta - ljet_1_eta);
+				h_lep_delta_eta->Fill(lep_0_eta - lep_1_eta);
+				h_ljet_delta_phi->Fill(ljet_0_phi - ljet_1_phi);
+				h_lep_delta_phi->Fill(lep_0_phi - lep_1_phi);
+				h_ljet_delta_R->Fill(DeltaR(ljet_0_p4,ljet_1_p4));
+				h_lep_delta_R->Fill(DeltaR(lep_0_p4,lep_1_p4));
+
+				// ------------------------- JETS --------------------------- //
+				h_n_jets->Fill(n_jets);
+
+				if (ljet_2) {
+				h_ljet_2_eta->Fill(ljet_2_eta);
+				h_ljet_2_phi->Fill(ljet_2_phi);
+				h_ljet_delta_eta_2->Fill(ljet_1_eta - ljet_2_eta);
+				}
+				if (ljet_3) {
+				h_ljet_3_eta->Fill(ljet_3_eta);
+				h_ljet_3_phi->Fill(ljet_3_phi);
+				h_ljet_delta_eta_3->Fill(ljet_2_eta - ljet_3_eta);
+				}
+
+				h_lep_eta_L_SL->Fill(lep_0_eta, lep_1_eta);
+				h_ljet_eta_L_SL->Fill(ljet_0_eta, ljet_1_eta);
+
+				if (n_jets == 2) {
+					h_ljet_0_eta_2jets->Fill(ljet_0_eta);
+					h_ljet_1_eta_2jets->Fill(ljet_1_eta);
+				}
+				if (n_jets == 3) {
+					h_ljet_0_eta_3jets->Fill(ljet_0_eta);
+					h_ljet_1_eta_3jets->Fill(ljet_1_eta);
+					h_ljet_2_eta_3jets->Fill(ljet_2_eta);
+				}
+				if (n_jets == 4) {
+					h_ljet_0_eta_4jets->Fill(ljet_0_eta);
+					h_ljet_1_eta_4jets->Fill(ljet_1_eta);
+					h_ljet_2_eta_4jets->Fill(ljet_2_eta);
+					h_ljet_3_eta_4jets->Fill(ljet_3_eta);
+				}
+				if (n_jets == 5) {
+					h_ljet_0_eta_5jets->Fill(ljet_0_eta);
+					h_ljet_1_eta_5jets->Fill(ljet_1_eta);
+					h_ljet_2_eta_5jets->Fill(ljet_2_eta);
+					h_ljet_3_eta_5jets->Fill(ljet_3_eta);
+				}
+
+				// ----------------------- Z BOSON -------------------------- //
+				double Z_cent = Centrality(lep_0_p4, lep_1_p4,
+											ljet_0_p4, ljet_1_p4);
 				h_Z_cent->Fill(Z_cent);
 
+
 			}
-			double dilep_inv_mass_NoCut = InvariantMass(lep_0_p4, lep_1_p4);			
-			if (lep_type == "Electron") h_elec_inv_mass_NoCut->Fill(dilep_inv_mass_NoCut);
-			if (lep_type == "Muon") h_muon_inv_mass_NoCut->Fill(dilep_inv_mass_NoCut);
-			if (lep_type == "Tau") h_tau_inv_mass_NoCut->Fill(dilep_inv_mass_NoCut);
-			if (lep_type == "ElectronMuon") h_elec_muon_inv_mass_NoCut->Fill(dilep_inv_mass_NoCut);
-			if (lep_type == "MuonTau") h_muon_tau_inv_mass_NoCut->Fill(dilep_inv_mass_NoCut);
-			if (lep_type == "ElectronTau") h_elec_tau_inv_mass_NoCut->Fill(dilep_inv_mass_NoCut);
-			
+			// NO Cuts
+			// --------------------- INVARIANT MASS --------------------- //
+			double dilep_inv_mass_NoCut = InvariantMass(lep_0_p4, lep_1_p4);
+			if (lep_type == "Electron") {
+				h_elec_inv_mass_NoCut->Fill(dilep_inv_mass_NoCut);
+			} if (lep_type == "Muon") {
+				h_muon_inv_mass_NoCut->Fill(dilep_inv_mass_NoCut);
+			} if (lep_type == "Tau") {
+				h_tau_inv_mass_NoCut->Fill(dilep_inv_mass_NoCut);
+			} if (lep_type == "ElectronMuon") {
+				h_elec_muon_inv_mass_NoCut->Fill(dilep_inv_mass_NoCut);
+			} if (lep_type == "MuonTau") {
+				h_muon_tau_inv_mass_NoCut->Fill(dilep_inv_mass_NoCut);
+			} if (lep_type == "ElectronTau") {
+				h_elec_tau_inv_mass_NoCut->Fill(dilep_inv_mass_NoCut);
+			}
+			// ------------------------ CONES ----------------------------//
 			h_elec_iso_etcone20->Fill(elec_0_iso_etcone20);
 			h_muon_iso_etcone20->Fill(muon_0_iso_etcone20);
 
 			h_elec_iso_ptcone30->Fill(elec_0_iso_ptcone30);
 			h_muon_iso_ptcone30->Fill(muon_0_iso_ptcone30);
-			
+
 			h_ljet_inv_mass->Fill(InvariantMass(ljet_0_p4, ljet_1_p4));
 			h_bjet_inv_mass->Fill(InvariantMass(bjet_0_p4, bjet_1_p4));
-			
-			double Z_cent_NoCut = Centrality(lep_0_p4, lep_1_p4, ljet_0_p4, ljet_1_p4);
+
+			// ----------------------- Z BOSON -------------------------- //
+			double Z_cent_NoCut = Centrality(lep_0_p4, lep_1_p4,
+				 								ljet_0_p4, ljet_1_p4);
 			h_Z_cent_NoCut->Fill(Z_cent_NoCut);
-	
-			double delta_eta_NoCut = ljet_0_eta - ljet_1_eta;
-			h_ljet_delta_eta_NoCut->Fill(delta_eta_NoCut);
-			
+
+			// ---------------------- KINEMATICS ------------------------ //
+			h_ljet_delta_eta_NoCut->Fill(ljet_0_eta - ljet_1_eta);
+			h_lep_delta_eta_NoCut->Fill(lep_0_eta - lep_1_eta);
+			h_ljet_delta_phi_NoCut->Fill(ljet_0_phi - ljet_1_phi);
+			h_lep_delta_phi_NoCut->Fill(lep_0_phi - lep_1_phi);
+			h_ljet_delta_R_NoCut->Fill(DeltaR(ljet_0_p4,ljet_1_p4));
+			h_lep_delta_R_NoCut->Fill(DeltaR(lep_0_p4,lep_1_p4));
+
+			int lep_n;
+			h_lep_n->Fill(lep_n);
 		}
-		int lep_n;
-		h_lep_n->Fill(lep_n);
-		
-		
-		h_elec_inv_mass->SetLineColor(4);
-		h_elec_inv_mass_NoCut->SetLineColor(3);
+
+
 
 
 	}
+	// ---------------------- HISTOGRAM STYLE --------------------------- //
+	h_elec_inv_mass->SetLineColor(4);
+	h_elec_inv_mass_NoCut->SetLineColor(3);
 
-	// Create file to write histograms
+	h_lep_eta_L_SL->GetXaxis()->SetTitle("Leading");
+	h_lep_eta_L_SL->GetYaxis()->SetTitle("Sub-leading");
+	h_ljet_eta_L_SL->GetXaxis()->SetTitle("Leading");
+	h_ljet_eta_L_SL->GetYaxis()->SetTitle("Sub-leading");
+
+
 	TFile outfile("outfile.root","RECREATE");
-	#include "Headers/WriteHistos.h"
+	#include "Headers/WriteHistos.h"		// Writes all histograms
 	outfile.Close();
 
 	cout << "Done!" << endl;
-	
-	// End of process time
+
+	// End of process time and print total time to terminal and ProcessTimes.txt
 	auto end = chrono::steady_clock::now();
-	double time_elapsed = chrono::duration_cast<chrono::seconds>(end - start).count();
-	cout << "Elapsed time: " << time_elapsed << " sec\n";
-	
+	int sec_elapsed = chrono::duration_cast<chrono::seconds>(end - start).count();
+	int min_elapsed = sec_elapsed / 60;
+	sec_elapsed = sec_elapsed - 60*min_elapsed;
+	cout << "Elapsed time: " << min_elapsed << " min " << sec_elapsed <<  " sec" << endl;
+
 	ofstream ProcessTimes_File;
 	ProcessTimes_File.open ("ProcessTimes.txt",ios_base::app);
-	ProcessTimes_File << time_elapsed << " s";
+	ProcessTimes_File << min_elapsed << ':' << sec_elapsed;
 	ProcessTimes_File.close();
 }
 
