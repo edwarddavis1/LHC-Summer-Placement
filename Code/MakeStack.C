c509ec790cf1237768edead90001c919aac4ffb0
edwarddavis1 <52565961+edwarddavis1@users.noreply.github.com>
2019-07-29 12:53:52 +0100

    Update MakeStack.C
    Tidied up the code a bunch - no new functionality

--- Code/MakeStack.C
+++ Code/MakeStack.C
@@ -3,17 +3,19 @@
 #include <iostream>
 #include "vector"
 
-void AddStack(TString c_name, TString get_hist, TString files[], int file_length, string MC_type, bool norm_hist=false) {
+void AddStack(TString c_name, TString get_hist, TString files[],
+			int file_length, string MC_type, bool norm_hist=false) {
 	TCanvas *c = new TCanvas(c_name);
 	THStack *hs = new THStack(c_name+"_stack","");
 	auto legend = new TLegend(0.75,0.7,0.99,0.99);
 	TString leg_title;
-	
+
 	for (UInt_t i=0;i<file_length;i++){
 		TFile* file = new TFile("OutputFiles/"+files[i]+".root");
 		TH1F* h = (TH1F*)file->Get(get_hist);
+
 		if (norm_hist == true) {
-			Double_t norm = h->GetEntries();	// Normalise histogram 
+			Double_t norm = h->GetEntries();
 			h->Scale(1/norm);
 			h->SetLineColor(i+2);
 			leg_title = get_hist+"_norm";
@@ -22,30 +24,33 @@ void AddStack(TString c_name, TString get_hist, TString files[], int file_length
 		leg_title=MC_type+" "+get_hist;
 		hs->Add(h);
 		legend->AddEntry(h,files[i],"f");
-	
+
 	hs->Draw();
 	hs->GetXaxis()->SetTitle(get_hist);
 	hs->GetYaxis()->SetTitle("Counts");
 	gPad->Modified();
-	gPad->Update();	
-	
-	legend->SetHeader(leg_title,"C"); // option "C" allows to center the header
+	gPad->Update();
+
+	legend->SetHeader(leg_title,"C");
 	legend->SetTextSize(0.02);
 	legend->Draw();
 	}
-	
-	TFile outfile(TString::Format("OutputFiles/Stacked_%s.root",MC_type.c_str()),"UPDATE");
+
+	TFile outfile(TString::Format("OutputFiles/Stacked_%s.root",
+									MC_type.c_str()),"UPDATE");
 	c->Write("",TObject::kOverwrite);
 	outfile.Close();
 }
 
-void StackCuts(TString c_name, TString get_hists[], TString file_name, int hists_length, string MC_type, bool norm_hist=false) {
+void StackCuts(TString c_name, TString get_hists[], TString file_name,
+				int hists_length, string MC_type, bool norm_hist=false) {
+
 	TCanvas *c = new TCanvas(c_name);
 	THStack *hs = new THStack(c_name+"_stack","");
 	TFile* file = new TFile("OutputFiles/"+file_name+".root");
 	auto legend = new TLegend(0.75,0.7,0.99,0.99);
 	TString leg_title;
-	
+
 	for (UInt_t i=0;i<hists_length;i++){
 		TH1F* h;
 		h = (TH1F*)file->Get(get_hists[i]);
@@ -54,31 +59,35 @@ void StackCuts(TString c_name, TString get_hists[], TString file_name, int hists
 		hs->Add(h);
 		legend->AddEntry(h,get_hists[i],"f");
 		//h->Reset();
-	
+
 	hs->Draw();
 	hs->GetYaxis()->SetTitle("Counts");
 	gPad->Modified();
-	gPad->Update();	
-	
-	legend->SetHeader(leg_title,"C"); // option "C" allows to center the header
+	gPad->Update();
+
+	legend->SetHeader(leg_title,"C");
 	legend->SetTextSize(0.02);
 	legend->Draw();
 	}
-	
-	TFile outfile(TString::Format("OutputFiles/Stacked_%s.root",MC_type.c_str()),"UPDATE");
+
+	TFile outfile(TString::Format("OutputFiles/Stacked_%s.root",
+									MC_type.c_str()),"UPDATE");
 	c->Write("",TObject::kOverwrite);
 	outfile.Close();
 }
 
 
-void PlotSameAxes(TString file_names[],int files_len, TString hists[], int hists_len, string MC_type, TString legend_title) {
+void PlotSameAxes(TString file_names[],int files_len, TString hists[],
+					int hists_len, string MC_type, TString legend_title,
+					bool write) {
+
 	TCanvas *c = new TCanvas(legend_title);
 	auto legend = new TLegend(0.75,0.7,0.99,0.99);
-	
-	
+
+
 	float temp_min=0, temp_max=0;
 	for (UInt_t j=0; j<files_len; j++) {
-		TFile* file = new TFile("OutputFiles/"+file_names[j]+".root");	
+		TFile* file = new TFile("OutputFiles/"+file_names[j]+".root");
 		for (UInt_t i=0; i<hists_len; i++) {
 			TH1F* h = (TH1F*)file->Get(hists[i]);
 			if ((i==0) && (j==0)) {
@@ -91,7 +100,7 @@ void PlotSameAxes(TString file_names[],int files_len, TString hists[], int hists
 			}
 		}
 	}
-	int colour = 6;
+	int colour = 2;
 	for (UInt_t j=0; j<files_len; j++) {
 		TFile* file = new TFile("OutputFiles/"+file_names[j]+".root");
 		for (UInt_t i=0; i<hists_len; i++) {
@@ -99,8 +108,7 @@ void PlotSameAxes(TString file_names[],int files_len, TString hists[], int hists
 			h->SetFillColor(colour);
 			colour += 1;
 			h->SetFillStyle(3001);
-			legend->AddEntry(h,file_names[j]+hists[i],"f");
-			cout << "Drawing " << file_names[j] << endl;
+			legend->AddEntry(h,file_names[j]+" "+hists[i],"f");
 			if ((i==0) && (j==0)) {
 				h->Draw();
 				h->SetMaximum(1.2*temp_max);
@@ -110,13 +118,18 @@ void PlotSameAxes(TString file_names[],int files_len, TString hists[], int hists
 		}
 	}
 	legend->SetHeader(legend_title,"C");
-	legend->SetTextSize(0.015);
+	legend->SetTextFont(42);
 	legend->Draw();
-	
-	TFile outfile(TString::Format("OutputFiles/SameAxes_%s.root",MC_type.c_str()),"UPDATE");
-	c->Write("",TObject::kOverwrite);
-	outfile.Close();
+	TLegendEntry *header = (TLegendEntry*)legend->GetListOfPrimitives()->First();
+	header->SetTextFont(42);
+	header->SetTextSize(0.025);
 
+	if (write == true) {
+		TFile outfile(TString::Format("OutputFiles/SameAxes_%s.root",
+						MC_type.c_str()),"UPDATE");
+		c->Write("",TObject::kOverwrite);
+		outfile.Close();
+		}
 }
 
 void MakeStack() {
@@ -124,28 +137,61 @@ void MakeStack() {
 	cout << "Making stacks..." << endl;
 
 	// All Zmumu datasets: 15
-	TString Zmm_[]={"Zmm2jets_Min_N_TChannel","Zmumu_MV70_140_BFil","Zmumu_MV70_140_CFilBVet","Zmumu_MV70_140_CVetBVet","Zmumu_MV140_280_BFil",
-			"Zmumu_MV140_280_CFilBVet","Zmumu_MV140_280_CVetBVet", "Zmumu_MV0_70_BFil","Zmumu_MV0_70_CFilBVet","Zmumu_MV0_70_CVetBVet",
-			"Zmumu_MV280_500_BFil","Zmumu_MV280_500_CFilBVet","Zmumu_MV280_500_CVetBVet","Zmumu_MV500_1000","Zmumu_MV1000_E_CMS"};
-	
+	TString Zmm_[]={"Zmm2jets_Min_N_TChannel", "Zmumu_MV70_140_BFil",
+				"Zmumu_MV70_140_CFilBVet", "Zmumu_MV70_140_CVetBVet",
+				"Zmumu_MV140_280_BFil", "Zmumu_MV140_280_CFilBVet",
+				"Zmumu_MV140_280_CVetBVet", "Zmumu_MV0_70_BFil",
+				"Zmumu_MV0_70_CFilBVet", "Zmumu_MV0_70_CVetBVet",
+				"Zmumu_MV280_500_BFil", "Zmumu_MV280_500_CFilBVet",
+				"Zmumu_MV280_500_CVetBVet", "Zmumu_MV500_1000",
+				"Zmumu_MV1000_E_CMS"};
+
 	// BFils and jets: 5
-	TString Zmm_BFil[]={"Zmm2jets_Min_N_TChannel","Zmumu_MV0_70_BFil","Zmumu_MV70_140_BFil","Zmumu_MV140_280_BFil","Zmumu_MV280_500_BFil"};
-	
+	TString Zmm_BFil[]={"Zmm2jets_Min_N_TChannel","Zmumu_MV0_70_BFil",
+						"Zmumu_MV70_140_BFil","Zmumu_MV140_280_BFil",
+						"Zmumu_MV280_500_BFil"};
+
 	// CVetBFil and jets: 5
-	TString Zmm_CFilBVet[]={"Zmm2jets_Min_N_TChannel","Zmumu_MV0_70_CFilBVet","Zmumu_MV70_140_CFilBVet","Zmumu_MV140_280_CFilBVet","Zmumu_MV280_500_CFilBVet"};
-	TString Zee_CFilBVet[]={"Zee2jets_Min_N_TChannel","Zee_MV0_70_CFilBVet","Zee_MV70_140_CFilBVet","Zee_MV140_280_CFilBVet","Zee_MV280_500_CFilBVet"};
-			
-	bool norm_hist=true;
-	
+	TString Zmm_CFilBVet[]={"Zmm2jets_Min_N_TChannel",
+							"Zmumu_MV280_500_CFilBVet",
+							"Zmumu_MV140_280_CFilBVet",
+							"Zmumu_MV70_140_CFilBVet",
+							"Zmumu_MV0_70_CFilBVet"};
+
+	TString Zee_CFilBVet[]={"Zee2jets_Min_N_TChannel",
+							"Zee_MV0_70_CFilBVet",
+							"Zee_MV140_280_CFilBVet",
+							"Zee_MV70_140_CFilBVet",
+							"Zee_MV280_500_CFilBVet"};
+
+	//---------------------------- Files --------------------------------//
+	TString Zmm2jets[] = {"Zmm2jets_Min_N_TChannel"};
+	TString Zmumu_MV280_500_CFilBVet[] = {"Zmumu_MV280_500_CFilBVet"};
+	TString Zmumu_MV140_280_CFilBVet[] = {"Zmumu_MV140_280_CFilBVet"};
+
+	//-------------------------- Histograms -----------------------------//
 	TString ljet_pt[] = {"ljet_0_pt","ljet_1_pt","ljet_2_pt", "ljet_3_pt"};
 	TString ljet_eta[] = {"ljet_0_eta","ljet_1_eta","ljet_2_eta","ljet_3_eta"};
 	TString ljet_phi[] = {"ljet_0_phi","ljet_1_phi","ljet_2_phi","ljet_3_phi"};
-	
-	TString ljet_test[] = {"ljet_0_eta","ljet_0_phi"};
-	
-	TString jets_280[] = {"Zmm2jets_Min_N_TChannel","Zmumu_MV280_500_CFilBVet", "Zmumu_MV140_280_CFilBVet"};
-	
-	PlotSameAxes(jets_280,3,ljet_test,2,"Zmm", "jets_280_ljet_pt");
+	TString ljet_delta_eta_2[] = {"ljet_delta_eta_2"};
+	TString ljet_delta_eta[] = {"ljet_delta_eta"};
+	TString ljet_delta_eta_3[] = {"ljet_delta_eta_3"};
+	TString ljet_0_eta_2jets[] = {"ljet_0_eta_2jets"};
+	TString ljet_1_eta_2jets[] = {"ljet_1_eta_2jets"};
+	TString ljet_0_eta_indi[] = {"ljet_0_eta_2jets", "ljet_0_eta_3jets",
+									"ljet_0_eta_4jets", "ljet_0_eta_5jets"};
+	TString ljet_1_eta_indi[] = {"ljet_1_eta_2jets", "ljet_1_eta_3jets",
+									"ljet_1_eta_4jets", "ljet_1_eta_5jets"};
+	TString ljet_2_eta_indi[] = {"ljet_2_eta_2jets", "ljet_2_eta_3jets",
+									"ljet_2_eta_4jets", "ljet_2_eta_5jets"};
+	TString ljet_3_eta_indi[] = {"ljet_3_eta_2jets", "ljet_3_eta_3jets",
+									"ljet_3_eta_4jets", "ljet_3_eta_5jets"};
+
+	bool write=true;
+	bool dont_write=false;
+
+	PlotSameAxes(Zmm2jets, 1, ljet_0_eta_indi, 4, "Zmm",
+					"Zmm2jets ljets 0&1 for 2jets", dont_write);
 
 
 	cout << "Stacks made!" << endl;
