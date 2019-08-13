4a7de2b784243adc2114aad004c599601c3ee1e9
edwarddavis1 <52565961+edwarddavis1@users.noreply.github.com>
2019-08-13 16:06:23 +0100

    Update MakeStack.C
    Added specific colouring, composition percentages, automatic TString for individual histograms

--- Code/MakeStack.C
+++ Code/MakeStack.C
@@ -5,18 +5,21 @@
 
 void PlotSameAxes(TString file_names[],int files_len, TString hists[],
 					int hists_len, string MC_type, TString legend_entries[],
-					TString axis_title, bool write,	bool log_scale) {
+					TString axis_title, TString region,
+					bool write,	bool logx_scale, bool logy_scale) {
 
-	TCanvas *c = new TCanvas(axis_title);
-	if (log_scale) c->SetLogy();
-	auto legend = new TLegend(0.60, 0.89, 0.9, 0.6);
+	int hist_num = hists_len + files_len;
+	TCanvas *c = new TCanvas(axis_title+"_"+region+"_stack");
+	if (logy_scale) c->SetLogy();
+	if (logx_scale) c->SetLogx();
+	auto legend = new TLegend(0.75, 0.89, 0.9, 0.9 - (hist_num - 1)*0.04);
 
 	float temp_min=0, temp_max=0;
 	for (UInt_t j=0; j<files_len; j++) {
 		TFile* file = new TFile("OutputFiles/"+file_names[j]+".root");
 		for (UInt_t i=0; i<hists_len; i++) {
 			TH1F* h = (TH1F*)file->Get(hists[i]);
-			if (log_scale) h->SetMinimum(1);
+			if (logy_scale) h->SetMinimum(1);
 			if ((i==0) && (j==0)) {
 				temp_min = h->GetMinimum();
 				temp_max = h->GetMaximum();
@@ -34,12 +37,37 @@ void PlotSameAxes(TString file_names[],int files_len, TString hists[],
 			TH1F* h = (TH1F*)file->Get(hists[i]);
 			h->SetStats(kFALSE);
 			h->SetTitle("");
-			h->SetLineColor(kBlack);
-			h->SetFillColor(colour);
-			colour += 1;
+
+			// could maybe remove this once a bunch of sims have been run
+			// GetColour() in GenericFunctions colours individual sims
+			if (file_names[j].Contains("VBF")) colour = 590;
+			else if (file_names[j].Contains("jets")) {
+				if (file_names[j].Contains("Zmm")) colour = 856;
+				if (file_names[j].Contains("Zee")) colour = 851;
+				if (file_names[j].Contains("Ztt")) colour = 853;
+			}
+			else if (file_names[j].Contains("ZqqZll")) colour = 806;
+			else if (file_names[j].Contains("Wenu")) colour = 820;
+			else if (file_names[j].Contains("Wmunu")) colour = 819;
+			else if (file_names[j].Contains("Wtaunu")) colour = 827;
+			else if (file_names[j].Contains("ttb")) colour = 632;
+			else if (file_names[j].Contains("llll")) colour = 908;
+			else if (file_names[j].Contains("lllv")) colour = 891;
+			else if (file_names[j].Contains("llvv")) colour = 906;
+			else if (file_names[j].Contains("lvvv")) colour = 901;
+			else if (file_names[j].Contains("Z")) colour = 865;
+
+			else colour = 0;
+
+			if (file_names[j].Contains("r9364")) colour = 595;
+
+			// h->SetLineColor(kBlack);
+			h->SetLineColor(colour);
+
 			legend->AddEntry(h,legend_entries[i+j],"f");
 			h->GetYaxis()->SetTitle("Events");
 			h->GetXaxis()->SetTitle(axis_title);
+
 			if ((i==0) && (j==0)) {
 				h->Draw("h");
 				h->SetMaximum(1.2*temp_max);
@@ -49,9 +77,23 @@ void PlotSameAxes(TString file_names[],int files_len, TString hists[],
 		}
 	}
 	legend->SetTextFont(42);
-	legend->SetTextSize(0.037);
+	legend->SetTextSize(0.03);
 	legend->SetBorderSize(0);
 	legend->Draw();
+
+	TText *region_label = new TText();
+	region_label-> SetNDC();
+	region_label -> SetTextFont(42);
+	region_label -> SetTextSize(0.03);
+	region_label -> SetTextAlign(22);
+	region_label -> DrawText(0.65, 0.86, region+" Region");
+
+	TText *data_label = new TText();
+	data_label-> SetNDC();
+	data_label -> SetTextFont(42);
+	data_label -> SetTextSize(0.03);
+	data_label -> SetTextAlign(22);
+	data_label -> DrawText(0.65, 0.8, "MC Simulation");
 
 	if (write == true) {
 		TFile outfile(TString::Format("OutputFiles/SameAxes_%s.root",
@@ -62,147 +104,200 @@ void PlotSameAxes(TString file_names[],int files_len, TString hists[],
 }
 
 void PlotStack(TString file_names[],int files_len, TString hists[],
-					int hists_len, string MC_type, TString legend_entries[],
-					TString axis_title, bool write,	bool log_scale) {
+					int hists_len, TString legend_entries[],
+					TString axis_title, TString region,
+					bool logx_scale, bool logy_scale) {
 
-	int entries = hists_len + files_len;
-	TCanvas *c = new TCanvas(axis_title+"_stack");
-	if (log_scale) c->SetLogy();
-	THStack *hs = new THStack(axis_title+"_stack", "");
-	auto legend = new TLegend(0.75, 0.89, 0.9, 0.9 - (entries - 1)*0.065);
+	int hist_num = hists_len + files_len;
+	TCanvas *c = new TCanvas(axis_title + "_" + region + "_stack");
+	if (logy_scale) c->SetLogy();
+	if (logx_scale) c->SetLogx();
+	THStack *hs = new THStack(axis_title + "_" + region + "_stack", "");
+	auto legend = new TLegend(0.75, 0.89, 0.9, 0.9 - (hist_num - 1)*0.04);
 
+	// get files
+	vector<Long64_t> hist_entries;
+	Long64_t sum_hist_entries = 0;
 	float temp_min=0, temp_max=0;
 	for (UInt_t j=0; j<files_len; j++) {
 		TFile* file = new TFile("OutputFiles/"+file_names[j]+".root");
 		for (UInt_t i=0; i<hists_len; i++) {
 			TH1F* h = (TH1F*)file->Get(hists[i]);
-			if (log_scale) h->SetMinimum(1);
+			Long64_t hist_entry = h -> GetEntries();
+			hist_entries.push_back(hist_entry);
+			sum_hist_entries += hist_entry;
+
+
+			if (logy_scale) h->SetMinimum(1);
 		}
 	}
-	int colour = 6;
+
+	// set style & draw
+	int colour;
 	for (UInt_t j=0; j<files_len; j++) {
 		TFile* file = new TFile("OutputFiles/"+file_names[j]+".root");
 		for (UInt_t i=0; i<hists_len; i++) {
 			TH1F* h = (TH1F*)file->Get(hists[i]);
 			h->SetStats(kFALSE);
 			h->SetTitle("");
+			h->GetXaxis()->SetRange(0, 200);
+
+			// could maybe remove this once a bunch of sims have been run
+			// GetColour() in GenericFunctions colours individual sims
+			if (hists_len == 1) {
+				if (file_names[j].Contains("VBF")) colour = 590;
+				else if (file_names[j].Contains("jets")) {
+					if (file_names[j].Contains("Zmm")) colour = 856;
+					if (file_names[j].Contains("Zee")) colour = 851;
+					if (file_names[j].Contains("Ztt")) colour = 853;
+				}
+				else if (file_names[j].Contains("ZqqZll")) colour = 806;
+				else if (file_names[j].Contains("Wenu")) colour = 400;
+				else if (file_names[j].Contains("Wmunu")) colour = 390;
+				else if (file_names[j].Contains("Wtaunu")) colour = 395;
+				else if (file_names[j].Contains("ttb")) colour = 632;
+				else if (file_names[j].Contains("llll")) colour = 908;
+				else if (file_names[j].Contains("lllv")) colour = 891;
+				else if (file_names[j].Contains("llvv")) colour = 906;
+				else if (file_names[j].Contains("lvvv")) colour = 901;
+				else if (file_names[j].Contains("QCD")) colour = 865;
+				else if (file_names[j].Contains("Zmumu")) colour = 840;
+				else if (file_names[j].Contains("Zee")) colour = 849;
+				else if (file_names[j].Contains("Ztt")) colour = 843;
+
+				else colour = 0;
+			}
+			else {
+				if (legend_entries[i+j].Contains("Pre-Select")) colour = 880;
+				else if (legend_entries[i+j].Contains("Baseline")) colour = 600;
+				else if (legend_entries[i+j].Contains("Search")) colour = 432;
+				else if (legend_entries[i+j].Contains("Control")) colour = 416;
+				else colour = 400;
+
+			}
+
+			if (file_names[j].Contains("r9364")) colour = 595;
+
 			h->SetLineColor(kBlack);
 			h->SetFillColor(colour);
-			colour += 1;
+
+
 			hs->Add(h);
 			hs->Draw("h");
-			legend->AddEntry(h,legend_entries[i+j],"f");
+			legend->AddEntry(h,legend_entries[i+j]);
 		}
 	}
-	// hs->Draw();
+
 	hs->GetYaxis()->SetTitle("Events");
 	hs->GetXaxis()->SetTitle(axis_title);
 
 	legend->SetTextFont(42);
-	legend->SetTextSize(0.037);
+	legend->SetTextSize(0.03);
 	legend->SetBorderSize(0);
 	legend->Draw();
 
-	if (write == true) {
-		TFile outfile(TString::Format("OutputFiles/SameAxes_%s.root",
-						MC_type.c_str()),"UPDATE");
-		c->Write("",TObject::kOverwrite);
-		outfile.Close();
-		}
+	TText *region_label = new TText();
+	region_label-> SetNDC();
+	region_label -> SetTextFont(42);
+	region_label -> SetTextSize(0.03);
+	region_label -> SetTextAlign(22);
+	region_label -> DrawText(0.65, 0.86, region+" Region");
+
+	TText *data_label = new TText();
+	data_label-> SetNDC();
+	data_label -> SetTextFont(42);
+	data_label -> SetTextSize(0.03);
+	data_label -> SetTextAlign(22);
+	data_label -> DrawText(0.65, 0.8, "MC Simulation");
+
+	// if (write == true) {
+	// 	TFile outfile(TString::Format("OutputFiles/SameAxes_%s.root",
+	// 					MC_type.c_str()),"UPDATE");
+	// 	c->Write("",TObject::kOverwrite);
+	// 	outfile.Close();
+	// 	}
+
+	// calculate percentages
+	cout << "\nComposition of " << region << " Region:" << endl;
+	for (int i = 0; i < hist_entries.size(); i++) {
+		double percentage;
+
+		percentage = 100 * ((double)hist_entries[i] / (double)sum_hist_entries);
+    	cout << setprecision(3);
+		cout << legend_entries[i] << ": " << percentage << " %" << endl;
+	}
+
+
 }
 
 void MakeStack() {
 	//gROOT->SetBatch(kTRUE);
 	cout << "Making stacks..." << endl;
 
-	// All Zmumu datasets: 15
-	TString Zmm_[]={"Zmm2jets_Min_N_TChannel", "Zmumu_MV70_140_BFil",
-				"Zmumu_MV70_140_CFilBVet", "Zmumu_MV70_140_CVetBVet",
-				"Zmumu_MV140_280_BFil", "Zmumu_MV140_280_CFilBVet",
-				"Zmumu_MV140_280_CVetBVet", "Zmumu_MV0_70_BFil",
-				"Zmumu_MV0_70_CFilBVet", "Zmumu_MV0_70_CVetBVet",
-				"Zmumu_MV280_500_BFil", "Zmumu_MV280_500_CFilBVet",
-				"Zmumu_MV280_500_CVetBVet", "Zmumu_MV500_1000",
-				"Zmumu_MV1000_E_CMS"};
-
-	// BFils and jets: 5
-	TString Zmm_BFil[]={"Zmm2jets_Min_N_TChannel","Zmumu_MV0_70_BFil",
-						"Zmumu_MV70_140_BFil","Zmumu_MV140_280_BFil",
-						"Zmumu_MV280_500_BFil"};
-
-	// CVetBFil and jets: 5
-	TString Zmm_CFilBVet[]={"Zmm2jets_Min_N_TChannel",
-							"Zmumu_MV280_500_CFilBVet",
-							"Zmumu_MV140_280_CFilBVet",
-							"Zmumu_MV70_140_CFilBVet",
-							"Zmumu_MV0_70_CFilBVet"};
-
-	TString Zee_CFilBVet[]={"Zee2jets_Min_N_TChannel",
-							"Zee_MV0_70_CFilBVet",
-							"Zee_MV140_280_CFilBVet",
-							"Zee_MV70_140_CFilBVet",
-							"Zee_MV280_500_CFilBVet"};
-
-	//---------------------------- Files --------------------------------//
-	TString Zmm2jets[] = {"Zmm2jets_Min_N_TChannel"};
-	TString Zmumu_MV280_500_CFilBVet[] = {"Zmumu_MV280_500_CFilBVet"};
-	TString Zmumu_MV140_280_CFilBVet[] = {"Zmumu_MV140_280_CFilBVet"};
-	TString VBF[] = {"VBF"};
-	TString VBF_r10201[] = {"VBF_r10201"};
-	TString EW[] = {"ttb_nonallh_r10201", "VBF_r10201", "Diboson", "Zll_QCD"};
-	TString EW_leg[] = {"t#bar{t}", "VBF", "Diboson", "Z#rightarrowll QCD"};
-	TString EW_QCD[] = {"VBF_r10201", "Zll_QCD"};
-	TString EW_QCD_leg[] = {"VBF", "Z#rightarrowll QCD"};
-
-	//-------------------------- Histograms -----------------------------//
-	TString ljet_pt[] = {"ljet_0_pt","ljet_1_pt","ljet_2_pt", "ljet_3_pt"};
-	TString ljet_eta[] = {"ljet_0_eta","ljet_1_eta","ljet_2_eta","ljet_3_eta"};
-	TString ljet_phi[] = {"ljet_0_phi","ljet_1_phi","ljet_2_phi","ljet_3_phi"};
-	TString ljet_delta_eta_2[] = {"ljet_delta_eta_2"};
-	TString ljet_delta_eta[] = {"ljet_delta_eta"};
-	TString ljet_delta_eta_3[] = {"ljet_delta_eta_3"};
-	TString ljet_0_eta_2jets[] = {"ljet_0_eta_2jets"};
-	TString ljet_1_eta_2jets[] = {"ljet_1_eta_2jets"};
-	TString ljet_0_eta_indi[] = {"ljet_0_eta_2jets", "ljet_0_eta_3jets",
-									"ljet_0_eta_4jets", "ljet_0_eta_5jets"};
-	TString ljet_1_eta_indi[] = {"ljet_1_eta_2jets", "ljet_1_eta_3jets",
-									"ljet_1_eta_4jets", "ljet_1_eta_5jets"};
-	TString ljet_2_eta_indi[] = {"ljet_2_eta_2jets", "ljet_2_eta_3jets",
-									"ljet_2_eta_4jets", "ljet_2_eta_5jets"};
-	TString ljet_3_eta_indi[] = {"ljet_3_eta_2jets", "ljet_3_eta_3jets",
-									"ljet_3_eta_4jets", "ljet_3_eta_5jets"};
-	TString ljets_phi[] = {"ljet_0_phi", "ljet_1_phi", "ljet_2_phi",
-							"ljet_3_phi"};
-	TString inv_mass_cuts[] = {"muon_inv_mass_preselect", "muon_inv_mass",
-								"muon_inv_mass_search", "muon_inv_mass_control",
-								"muon_inv_mass_high_mass"};
-	TString centrality[] = {"Z_cent", "Z_cent_search",
-							"Z_cent_control", "Z_cent_high_mass"};
-	TString ljet_inv_mass[] = {"ljet_inv_mass", "ljet_inv_mass_search"};
-	TString ljet_inv_mass_preselect[] = {"ljet_inv_mass_preselect"};
-	TString ljet_inv_mass_search[] = {"ljet_inv_mass_search"};
-	TString ljet_inv_mass_control[] = {"ljet_inv_mass_control"};
+	#include "Headers/TStringHistos.h"
 
 	bool write = true;
 	bool dont_write = false;
-	bool log_scale = true;
-	bool lin_scale = false;
+	bool logy_scale = true;
+	bool liny_scale = false;
+	bool logx_scale = true;
+	bool linx_scale = false;
 
-	TString regions[] = {"Baseline", "Search"};
 
+	//---------------------------- Files --------------------------------//
+	TString VBF[] = {"Zmm2jets_Min_N_TChannel_r10201",
+							"Zee2jets_Min_N_TChannel_r10201",
+							"Ztt2jets_Min_N_TChannel_r10201"};
+	TString Zlls[] = {"Zmm2jets_Min_N_TChannel_r10201", "Zmumu"};
+	TString Zlls_leg[] = {"EW Zll", "QCD Zll"};
 
-	// PlotSameAxes(Zmm2jets, 1, centrality, 4, "Zmm",
-	// 				"Zmm2jets Centrality", regions, "Centrality", dont_write,
-					// line_scale);
+	TString EW_QCD[] = {"ttb_nonallh_r10201",
+						"lvvv_r10201", "llvv_r10201",
+						"lllv_r10201", "llll_r10201",
+						"Wtaunu", "Wmunu", "Wenu",
+						"ZqqZll_r10201",
+						"VBF",
+						"Zll_QCD"};
 
-	// PlotSameAxes(EW, 3, ljet_inv_mass_preselect, 1, "EW",
-	// 				EW_leg, "mjj_r10201 [GeV/c^{2}]", dont_write, log_scale);
+	TString EW_QCD_leg[] = {"t#bar{t}",
+							"lvvv", "llvv", "lllv", "llll",
+							"Wtaunu", "Wmunu", "Wenu",
+							"ZqqZll",
+							"EW Zll",
+						 	"QCD Zll"};
 
-	PlotStack(EW, 4, ljet_inv_mass_search, 1, "EW",
-					EW_leg, "mjj search [GeV/c^{2}]", dont_write, log_scale);
+	TString all_processes[] = {"ttb_nonallh_r10201",
+						"lvvv_r10201", "llvv_r10201",
+						"lllv_r10201", "llll_r10201",
+						"Wtaunu", "Wmunu", "Wenu",
+						"ZqqZll_r10201",
+						"Zmm2jets_Min_N_TChannel_r10201",
+						"Zee2jets_Min_N_TChannel_r10201",
+						"Ztt2jets_Min_N_TChannel_r10201",
+						"Zmumu", "Zee", "Ztt"};
 
-	// PlotStack(EW_QCD, 2, ljet_inv_mass_control, 1, "EW",
-	// 				EW_QCD_leg, "mjj control [GeV/c^{2}]", dont_write, log_scale);
+	TString all_processes_leg[] = {"t#bar{t}",
+						"lvvv", "llvv",
+						"lllv", "llll",
+						"Wtaunu", "Wmunu", "Wenu",
+						"ZqqZll",
+						"EW Z#mu#mu",
+						"EW Zee",
+						"EW Ztt",
+						"QCD Zmumu", "QCD Zee", "QCD Ztt"};
+
+	//-------------------------- Histograms -----------------------------//
+	TString EW_QCD_test[] = {"VBF_r9364", "VBF_r10201", "Zll_QCD"};
+
+	TString QCD_Zll[] = {"Zmumu", "Zee", "Ztt"};
+	TString regions[] = {"Control", "Search", "Baseline", "Pre-Select"};
+
+	TString ljet_inv_mass_search = {"ljet_inv_mass_search"};
+
+	PlotStack(VBF, 1, ljet_inv_mass_search, 1,
+					VBF, "mll [GeV/c^{2}]", "Pre-Selection",
+					linx_scale, logy_scale);
+
 
 	cout << "Stacks made!" << endl;
 	gROOT->SetBatch(kFALSE);
